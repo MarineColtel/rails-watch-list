994223a8a3ca02fe7941faa48ad1c6afb6b1ac1b
Marine Coltel <marine_coltel@hotmail.fr>
2023-03-16 11:06:11 +0100

    Watch list creation and installation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Front-end style imports
+import "bootstrap"
+import "@popperjs/core"
