cd17ff07670b2288eafd80bc925807c3a8f655c6
Marine Coltel <marine_coltel@hotmail.fr>
2023-03-17 15:30:58 +0100

    watch list prod

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,7 +9,7 @@ class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list_id = @list
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
